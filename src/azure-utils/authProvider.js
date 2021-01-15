acfc13f15c79712021d24bb9850a3230f4b8165f
sundhar bharath kamanuru <sunderbharath85@live.com>
2021-01-15 02:50:32 -0800

    Update url

--- src/azure-utils/authProvider.js
+++ src/azure-utils/authProvider.js
@@ -2,7 +2,7 @@ import { MsalAuthProvider, LoginType } from "react-aad-msal";
 const tenant = "collegehubapp.onmicrosoft.com";
 const signInPolicy = "B2C_1_sign_in_sign_up";
 const applicationID = "f8448b53-2326-4c77-911d-343d80d393e7";
-const reactRedirectUri = "http://localhost:3000";
+const reactRedirectUri = "https://icy-sky-063a5dc1e.azurestaticapps.net";
 const tenantSubdomain = tenant.split(".")[0];
 const instance = `https://${tenantSubdomain}.b2clogin.com/tfp/`;
 const signInAuthority = `${instance}${tenant}/${signInPolicy}`;
